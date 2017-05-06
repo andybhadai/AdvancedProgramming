16a222640708bd53f8faa6ced0f259b4d32f2d8a
Andy Bhadai <abhadai@gmail.com>
2017-05-06 20:36:56 +0200

    Simple function with string parameter

--- Samples/Samples/Program.fs
+++ Samples/Samples/Program.fs
@@ -1,7 +1,3 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿let greeting (name : string) = printf "Hello, %s" name
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let test = greeting("Test")
