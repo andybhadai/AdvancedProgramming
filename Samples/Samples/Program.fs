6ad5913ad53a0f6447d1e762b26dbcc9b546ad28
Andy Bhadai <abhadai@gmail.com>
2017-06-10 17:16:43 +0200

    Àdded Option sample

--- Samples/Samples/Program.fs
+++ Samples/Samples/Program.fs
@@ -1,3 +1,19 @@
-﻿let greeting (name : string) = printf "Hello, %s" name
+﻿open System
 
-let test = greeting("Test")
+//let greeting (name : string) = printf "Hello, %s" name
+//let result1 = greeting("Andy Bhadai")
+
+// Some and None to check whether or not a number <= 0
+//let printNumber (n : int) = 
+//    match n with
+//    | 0 -> None
+//    | _ -> Some(n)
+
+//let result2 : int option = printNumber(1)
+//printfn "Result: %A" result2
+
+//let averageFour(a : int) (b : int) (c : int) (d : int)=
+//    let result = (a + b + c + d) / 4
+//    printf "The average of these is %i" result"
+
+    
